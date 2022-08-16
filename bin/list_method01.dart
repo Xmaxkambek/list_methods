a52af741b299879d7fc387963a07717d3e0aa42c
Maxkambek <maxkambekxolbekov515@gmail.com>
2022-08-16 08:24:51 +0500

    01

--- bin/list_method01.dart
+++ bin/list_method01.dart
@@ -1,11 +1,15 @@
-/*
-  create function with arguments
-      You will be given a list of fruits. Add x fruit to it from the end and return.
-    Args:
-        fruits(list): parameter 
-        x(str): parameter
-    Returns:
-        list: return answer
-*/
 
-void main() {}
+List func(List list1,List list2){
+  List x = list1+list2;
+  list1.add(x);
+  return x;
+}
+
+
+
+
+
+void main() {
+  print(func(['olma','anor','nok'],['banan']));
+  
+}
