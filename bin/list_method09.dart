4b1baa2acbfac2a88944dbc7f3c343dd2199a98a
Maxkambek <maxkambekxolbekov515@gmail.com>
2022-08-17 10:48:36 +0500

    09

--- bin/list_method09.dart
+++ bin/list_method09.dart
@@ -6,5 +6,25 @@
     Returns:
         list: return answer
 */
-
-void main() {}
+List func(List fruits){
+  int a = 0;
+  int b = 0;
+  int c = 0;
+  List d = [1];
+  int i = fruits.length;
+  while (i > a) {
+    if (fruits[a] == 'apple') {
+      b+=1;
+    }
+    a+=1;
+    if (fruits[c] == 'apple') {
+      d.add(c);
+    }
+    c+=1;
+  }
+  d[0] = b;
+  return d; 
+}
+void main() {
+  print(func(['apple','apple','apple','apple','kiwi']));
+}
